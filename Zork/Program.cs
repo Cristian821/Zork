8fc8b827596a6b597da6d3843db75e14828d2cf5
Cristian821 <55367336+Cristian821@users.noreply.github.com>
2020-09-15 23:32:34 -0400

    Zork 1.2
    Finding Direction

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 namespace Zork
 {
@@ -9,20 +10,49 @@ namespace Zork
             Console.WriteLine("Welcome to Zork!");
 
             string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
 
-            if (inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing");
-            }
-            else if (inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field west of a white house, with boarded front door. A rubber mat saying 'Welcome to Zork!' lies by the door.");
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command");
-            }
+        }
+        private static Commands ToCommand(string commandString)
+        {
+
+            return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+
+            //Commands command;
+            //switch (commandString)
+            //{
+            //    case "QUIT":
+            //        command = Commands.QUIT;
+            //        break;
+
+            //    case "LOOK":
+            //        command = Commands.LOOK;
+            //        break;
+
+            //    case "NORTH":
+            //        command = Commands.NORTH;
+            //        break;
+
+            //    case "SOUTH":
+            //        command = Commands.SOUTH;
+            //        break;
+
+            //    case "EAST":
+            //        command = Commands.EAST;
+            //        break;
+
+            //    case "WEST":
+            //        command = Commands.WEST;
+            //        break;
+
+            //    default:
+            //        command = Commands.UNKNOWN;
+            //        break;
+            //};
+
+            //return command;
         }
     }
 }
+
